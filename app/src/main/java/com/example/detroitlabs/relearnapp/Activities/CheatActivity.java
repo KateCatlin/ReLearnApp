1f84bc820bbb00b4922607b7cbfce97422feb2b9
KATE CATLIN <kate.catlin@detroitlabs.com>
2015-10-02 11:05:26 -0600

    Adding CheatActivitiy

---
+++ app/src/main/java/com/example/detroitlabs/relearnapp/Activities/CheatActivity.java
@@ -0,0 +1,9 @@
+package com.example.detroitlabs.relearnapp.Activities;
+
+import android.app.Activity;
+
+/**
+ * Created by DetroitLabs on 10/2/15.
+ */
+public class CheatActivity extends Activity {
+}
